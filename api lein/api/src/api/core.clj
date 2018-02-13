22651059d9a040b4958949be4a8719f6346da22f
Xavier <xavi.ametller@hotmail.com>
2018-02-14 00:06:43 +0100

    Created a bunch of functions in core.clj to find a file, a folder, list elements in a folder, enrich the response with a map and finally write it in JSON format. Still WIP.

--- api lein/api/src/api/core.clj
+++ api lein/api/src/api/core.clj
@@ -1,7 +1,43 @@
 (ns api.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json])
+  (:require [clojure.java.io :as io]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+((defn file-or-folder
+  "docstring"
+  [path]
+   (str "hello")
+  ))
+
+(defn fileOrFolder [path] (str "Hello, " path) )
+
+(defn isFile [path]
+  (
+    (.exists (io/as-file path))))
+
+
+; Works
+(defn isFile [path]
+  (.exists (io/as-file path)))
+
+; No need to define an extra function for is Directory
+' (.isDirectory (io/file "c:/ToDelete"))
+
+; function to list all elements in folder:
+(.list (io/file "c:/ToDelete"))
+
+
+(defn enrich [name]
+  (if (isFile path)
+    {:path path
+     :type "file"}
+    {:path path
+     :type "folder"})
+  )
+
+; (json/write-str (map enrich (.list (io/file "c:"))))
