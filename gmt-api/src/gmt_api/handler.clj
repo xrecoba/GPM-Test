c3eba57eba1654c6fbf33df53fb7e479d30428d0
Xavier <xavi.ametller@hotmail.com>
2018-02-15 22:25:56 +0100

    Updated project to return a list of files in c:/ToDelete folder when requesting /dir endpoint.

--- gmt-api/src/gmt_api/handler.clj
+++ gmt-api/src/gmt_api/handler.clj
@@ -1,11 +1,37 @@
 (ns gmt-api.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.java.io :as io]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]))
+
+(defn isFile [path]
+  (.exists (io/as-file path)))
+
+(defn enrich [path]
+  (if (isFile path)
+    {:path path
+     :type "file"}
+    {:path path
+     :type "folder"})
+  )
+
+;(defn listItems [path]
+; (.list (clojure.java.io/file path)))
+
+
+
+(defn handler [request]
+  (response (map enrich (.list (io/file "c:/ToDelete")))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+           (GET "/" [] "Hello World")
+           (GET "/dir" [] (wrap-json-response handler))
+           ;(GET "/dir" []  (wrap-json-response  {:value (+ 2 2)} ))
+
+           (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
