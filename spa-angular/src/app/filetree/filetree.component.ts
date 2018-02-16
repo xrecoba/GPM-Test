0b9f48d9c0b07748f8223773c74549ac953aaa88
Xavier <xavi.ametller@hotmail.com>
2018-02-16 23:21:34 +0100

    Added component and rendered it following the tutorial.

---
+++ spa-angular/src/app/filetree/filetree.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filetree',
+  templateUrl: './filetree.component.html',
+  styleUrls: ['./filetree.component.css']
+})
+export class FiletreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  hero = 'Windstorm';
+
+}
