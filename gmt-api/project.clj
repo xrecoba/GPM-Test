451eaa11e434868452d0ac577a91c5a3e5b85198
Xavier <xavi.ametller@hotmail.com>
2018-02-17 17:01:01 +0100

    Enabled localhost access from Angular by adding CORS to localhost:3000 in Clojure API.

--- gmt-api/project.clj
+++ gmt-api/project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [ring/ring-json "0.4.0"]]
+                 [ring/ring-json "0.4.0"]
+                 [jumblerg/ring.middleware.cors "1.0.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler gmt-api.handler/app}
   :profiles
