3820a69da28440d8397d9eb325314bbedf015572
Xavier <xavi.ametller@hotmail.com>
2018-02-21 22:41:51 +0100

    Simplified clojure handler code and changed it to return full path, file name and if it is directory or not.

--- gmt-api/src/gmt_api/handler.clj
+++ gmt-api/src/gmt_api/handler.clj
@@ -7,39 +7,20 @@
             [ring.util.response :refer [response]]
             [ring.middleware.cors :refer [wrap-cors]]))
 
-(defn isFile [path]
-  (.exists (io/as-file path)))
+(defn ToFileInfo [file]
+  (select-keys (bean file) [:path :name :directory]))
 
-;; accept everything
-;;(wrap-cors routes #".*")
-;;(wrap-cors routes identity)
-
-(defn enrich [path]
-  (if (isFile path)
-    {:path path
-     :type "file"}
-    {:path path
-     :type "folder"})
-  )
-
-;(defn listItems [path]
-; (.list (clojure.java.io/file path)))
-
-
-
-(defn handler [request]
-  (response (map enrich (.list (io/file "c:/ToDelete")))))
+(defn GetFiles [request]
+  (response (map ToFileInfo (.listFiles (io/file "c:/ToDelete")))))
 
 (defroutes app-routes
-           (GET "/" [] "Hello World")
-           (GET "/dir" [] (wrap-json-response handler))
-           ;(GET "/dir" []  (wrap-json-response  {:value (+ 2 2)} ))
+             (GET "/" [] "Hello World")
+             (GET "/dir" [] (wrap-json-response GetFiles))
 
-           (route/not-found "Not Found"))
+             (route/not-found "Not Found"))
 
 (def app
-  (-> (wrap-defaults app-routes site-defaults)
-      (wrap-cors :access-control-allow-origin #"http://localhost:4200"
-                 :access-control-allow-methods [:get]
-                 :access-control-allow-headers ["Content-Type"])))
-
+    (-> (wrap-defaults app-routes site-defaults)
+        (wrap-cors :access-control-allow-origin #"http://localhost:4200"
+                   :access-control-allow-methods [:get]
+                   :access-control-allow-headers ["Content-Type"])))
