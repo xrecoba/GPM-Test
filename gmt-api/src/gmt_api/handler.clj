1eeab3cc8206252383a0dad8cf7d087ce6ed0e07
Xavier <xavi.ametller@hotmail.com>
2018-02-22 18:13:01 +0100

    clean-up of trials

--- gmt-api/src/gmt_api/handler.clj
+++ gmt-api/src/gmt_api/handler.clj
@@ -11,18 +11,9 @@
 (defn ToFileInfo [file]
   (select-keys (bean file) [:path :name :directory]))
 
-(defn GetFiles [request]
-  ;(.println java.lang.System/out request)
-  (response (map ToFileInfo (.listFiles (io/file "c:/ToDelete")))))
-
-(defn GetFilesWithParam [path]
-  (.println java.lang.System/out path)
-  (.println java.lang.System/out (type path))
-
-  (map ToFileInfo (.listFiles (io/file path)))
-  ;(response  (+ 1 1))
-  ;(response (str "Hello user "))
-  )
+(defn GetFiles [path]
+    (map ToFileInfo (.listFiles (io/file path)))
+)
 
 (defn getFileLines [n filename]
   (let [fileContents (with-open [rdr (io/reader filename)]
@@ -32,21 +23,9 @@
 
 (defroutes app-routes
            (GET "/" [] "Hello World")
-           (GET "/preview" [path]  (getFileLines 10 "c:/ToDelete/Hola.txt"))
-           ;(GET "/previewWithParam" [path]  (getFileLines 10 path))
-           (GET "/previewWithParam" [path]  (json-response (getFileLines 10 path)))
-           ;           (GET "/id"  (wrap-json-response  (GetFilesWithParam "c:/ToDelete") ))
-
-           ;(GET "/:id" [u :uri]  (wrap-json-response  (GetFilesWithParam u) ))
-           (GET "/dirOld" [] (wrap-json-response GetFiles))
-           (GET "/dir" [path] (wrap-json-response (GetFilesWithParam path)))
-           (GET "/dirNew" [path] (wrap-json-response (fn [_] (response {:id path :message "hello2"}))))
-           (GET "/dirNew2" [path] (wrap-json-response (fn [_] (response (GetFilesWithParam path)))))
-
-           (GET "/HelloWorld" [path]  (str "Hello World" path)) ;Works
-           ;(GET "/dir" [] (wrap-json-response GetFiles))
-           (route/not-found "Not Found Yes"))
-
+           (GET "/dir" [path] (wrap-json-response (fn [_] (response (GetFiles path)))))
+           (GET "/preview" [path]  (json-response (getFileLines 10 path)))
+           (route/not-found "Not Found"))
 
 (def app
     (-> (wrap-defaults app-routes site-defaults)
