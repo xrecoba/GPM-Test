ace708aba64ba50eb61dd5e51dd9c69f8347452b
Xavier <xavi.ametller@gmail.com>
2018-02-24 00:11:22 +0100

    Added logic to allow user to specify root folder to start navigation.

--- spa-angular/src/app/app.component.ts
+++ spa-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { NodeService } from './node.service';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -7,15 +7,28 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GMT file explorer';
+export class AppComponent implements OnInit  {  
   previewFile: string;  
+  rootUrl: string;
   subscription: Subscription;
+  @Input() rootNode: string;
+  rootNodeIsSet: boolean;
 
   constructor(private nodeService: NodeService) {
+    this.rootNode = "c:/"    
     this.subscription = nodeService.fileToPreview$.subscribe(
       file => {        
         this.previewFile = file;           
     });
   }  
+
+  ngOnInit() {    
+    this.rootNodeIsSet = false;
+  }
+
+  setRootNode()
+  {        
+    this.rootUrl = "http://localhost:3000/dir?path=" + this.rootNode;
+    this.rootNodeIsSet = true;
+  } 
 }
